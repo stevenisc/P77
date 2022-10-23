d4bbce464a1d2c8c5f08f8ba90af4bbc12a9c7d7
stevenisc <114077756+stevenisc@users.noreply.github.com>
2022-10-23 12:01:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var first_name = "Ranbir";
+var last_name = "Kapoor";
+var a = 10;
+var b = 5;
+full_name = first_name.concat(last_name);
+console.log(full_name);
+add = a + b;
+console.log(add);
+subtract = a - b;
+console.log(subtract);
